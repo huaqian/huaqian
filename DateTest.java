122791c197d7065f863f78dee994a03f8c5727a9
huaqian <qianhua.az@gmail.com>
2022-01-25 23:40:49 -0700

    Adding data test

---
+++ DateTest.java
@@ -0,0 +1,24 @@
+package ownTest;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateTest {
+
+	public static void main(String[] args) throws Exception {
+		DateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		Date date = sdf.parse("02-10-2001");
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		System.out.println(calendar.get(Calendar.YEAR));
+		System.out.println(calendar.get(Calendar.MONTH));
+		System.out.println(calendar.get(Calendar.DAY_OF_MONTH));
+		
+		ZonedDateTime zdt = ZonedDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+//		System.out.println(zdt.get(ChronoField.YEAR));
+	}
+}
